ffa13d1175b943dbcf669c815e04c799c8143077
@k$hay <akshaynaik5787@gmail.com>
2023-07-19 09:32:51 +0530

    changing the state to add favourites as well

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,16 @@
 // {
 //     type: 'DECREASE_COUNT'
 // }
+
+
+
+// Action types
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+// Action Creators  
+export function addMovies (movies) {
+    return {
+        type: ADD_MOVIES,
+        movies
+      }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import { addMovies } from '../actions';
 
 class App extends React.Component {
 
@@ -15,15 +16,12 @@ class App extends React.Component {
     // make api call
 
     // Dispatch action
-    store.dispatch ({
-      type: 'ADD_MOVIES',
-      movies: data
-    })
+    store.dispatch (addMovies(data));
   }
 
   render() {
 
-    const movies = this.props.store.getState();
+    const { list } = this.props.store.getState();
 
     return (
       <>
@@ -36,7 +34,7 @@ class App extends React.Component {
             </div>
 
             <div className="list">
-              {movies.map((movie, index) => (
+              {list.map((movie, index) => (
                 <MovieCard movie={movie} key={`movies-${index}`}/>
               ))}
             </div>
